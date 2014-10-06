294323e8a492da8fa7598d0695cb0d2519f6fb6a
jipoom <jipoom@gmail.com>
2014-10-06 09:08:21 +0700

    Dashboard link

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,7 @@
 	<div class="welcome">
 		<p></p>
 		{{ HTML::link(URL::to('user/create'), 'Create users')}}<p></p>
-
+		{{ HTML::link(URL::to('report/dashboard'), 'Dashboard')}}<p></p>
 		{{ HTML::link(URL::to('report/poc'), 'MAP POC')}}<p></p>
 		{{ HTML::link(URL::to('report/stand_alone'), 'Stand Alone POC')}}
 	</div>
