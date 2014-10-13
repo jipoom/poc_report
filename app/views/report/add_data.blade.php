1c59d2238528fed6e6e5a72fa992d11851715c5e
jipoom <jipoom@gmail.com>
2014-10-13 09:17:23 +0700

    pre modify calendar format

--- app/views/report/add_data.blade.php
+++ app/views/report/add_data.blade.php
@@ -215,6 +215,24 @@
 		  $(function() {
 		    $("#found_date").datepicker({ dateFormat: 'dd-mm-yy',isBuddhist: true });
 		  });
+		  /*$(function () {
+		    var d = new Date();
+		    var toDay = d.getDate() + '/'
+        + (d.getMonth() + 1) + '/'
+        + (d.getFullYear() + 543);
+
+				// Datepicker
+		    $("#found_date").datepicker({ dateFormat: 'dd/mm/yy', isBuddhist: true, defaultDate: toDay, dayNames: ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์'],
+              dayNamesMin: ['อา.','จ.','อ.','พ.','พฤ.','ศ.','ส.'],
+              monthNames: ['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'],
+              monthNamesShort: ['ม.ค.','ก.พ.','มี.ค.','เม.ย.','พ.ค.','มิ.ย.','ก.ค.','ส.ค.','ก.ย.','ต.ค.','พ.ย.','ธ.ค.']});
+			  $("#datepicker-th-2").datepicker({ changeMonth: true, changeYear: true,dateFormat: 'dd/mm/yy', isBuddhist: true, defaultDate: toDay,dayNames: ['อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์'],
+              dayNamesMin: ['อา.','จ.','อ.','พ.','พฤ.','ศ.','ส.'],
+              monthNames: ['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'],
+              monthNamesShort: ['ม.ค.','ก.พ.','มี.ค.','เม.ย.','พ.ค.','มิ.ย.','ก.ค.','ส.ค.','ก.ย.','ต.ค.','พ.ย.','ธ.ค.']});
+			  $("#datepicker-en").datepicker({ dateFormat: 'dd/mm/yy'});
+			  $("#inline").datepicker({ dateFormat: 'dd/mm/yy', inline: true });
+			});*/
 
 		  </script>
 @stop
