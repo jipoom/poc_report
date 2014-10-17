aad737fc8da14629751168f300f2f96f64006a25
jipoom <jipoom@gmail.com>
2014-10-17 13:25:26 +0700

    finishing up DB and users added

--- app/controllers/user/UserController.php
+++ app/controllers/user/UserController.php
@@ -70,15 +70,20 @@ class UserController extends BaseController {
 		return Redirect::to('/')->with('message','You have logged out!!');		
 	}
 	public function getCreateBulkUsers(){
-		User::add('popsuri','calmvg','รจ.จ.ศรีสะเกษ');
-		User::add('popamna','7p5rkj','รจ.ก.สุรินทร์');
-		User::add('temsuri','bj46mv','รจ.ช.โคกตาปัน');
-		User::add('popamna','7p5rkj','รจ.จ.อำนาจเจริญ');
-		User::add('popubon','9kz4hj','รจ.ก.อุบลราชธานี');
-		User::add('aopyai','15gfuj','รจ.อ.บัวใหญ่');
-		User::add('idratta','e437jl','รจ.อ.รัตนบุรี');
-		User::add('aopkant','jpwuwm','รจ.อ.กันทรลักษ์');
-		User::add('fdcnakon','hu1g7v','ทส.ญ.นครราชสีมา');
+		User::add('poptran','nwq3zq','รจ.จ.ตรัง');
+		User::add('popnara','5dew3s','รจ.จ.นราธิวาส');
+		User::add('poppatt','v2whpd','รจ.ก.ปัตตานี');
+		User::add('coppata','jmdeau','รจ.ก.พัทลุง');
+		User::add('coppata1','f8n4zw','รจ.พ.พัทลุง');
+		User::add('odcbanna','bjmhry','ทส.ป.บ้านนาวง');
+		User::add('copyala','3uq2cy','รจ.ก.ยะลา');
+		User::add('aopbato','6e9wnt','รจ.อ.เบตง');
+		User::add('copsong','efrq8h','รจ.ก.สงขลา');
+		User::add('popsong','zt4yp5','รจ.จ.สงขลา');
+		User::add('fdcsong','zwf9sc','ทส.ญ.สงขลา');
+		User::add('tdcsong','88b2u4','ทส.บ.สงขลา');
+		User::add('popsatu','9qtexs','รจ.จ.สตูล');
+		User::add('aopnatv','aerb7v','รจ.อ.นาทวี');
 		//User::add('inctrad','bj46mv','สถานกักขังกลางจังหวัดตราด');
 	}
 
--- app/models/Item.php
+++ app/models/Item.php
@@ -15,14 +15,13 @@ class Item extends Eloquent {
 	public static function getAllItemArray()
 	{
 		$item= array();	
-		$init_item = Item::where('id','<>',0)->where('id','<>',1)->first();
+		$init_item = Item::where('id','<>',0)->first();
 		$item = array_add($item,$init_item->id, $init_item->name);
-		$allitem = Item::where('id','<>',0)->where('id','<>',1)->get();
+		$allitem = Item::where('id','<>',0)->get();
 		foreach($allitem as $temp)
 		{
 			$item = array_add($item, $temp->id, $temp->name);
 		}
-		$item = array_add($item, 1, 'อื่นๆ');
 		return $item;
 	}
 }
