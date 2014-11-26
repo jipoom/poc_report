38fdc627ef412157c1d5d2021d9df0368cbabeee
unknown <Jipoom@.(none)>
2014-11-26 22:18:11 +0700

    .

--- app/routes.php
+++ app/routes.php
@@ -22,10 +22,9 @@ Route::group(array('prefix' => 'report', 'before' => 'auth'), function()
 {
 	Route::get('poc', 'ReportController@showPOC');
 	Route::get('stand_alone', 'ReportController@showStandAlone');
-	Route::get('dashboard/{startDate?}/{endDate?}', 'ReportController@getDashBoard');
-	Route::post('dashboard', 'ReportController@postDashBoard');
 	Route::get('hdashboard', 'ReportController@showHDashBoard');
-	//อธิบดี
+	
+	//admin view report
 	Route::post('view_all', 'TableController@postAdminReport');
 	Route::get('view_all/{startDate?}/{endDate?}', 'TableController@getAdminReport');
 	Route::post('view_category', 'TableController@postAdminReportCategory');
@@ -50,6 +49,10 @@ Route::group(array('prefix' => 'report', 'before' => 'auth'), function()
 	Route::get('export', 'ReportController@export');
 	Route::get('getData/{startDate}/{endDate}/{location_id}/{found_at}', 'ReportController@getData');
 	Route::get('getDashBoardData/{date}/{khetId}', 'ReportController@getDashBoardData');
+		
+	//DashBoard
+	Route::get('dashboard/{startDate?}/{endDate?}', 'ReportController@getDashBoard');
+	Route::post('dashboard', 'ReportController@postDashBoard');
 	Route::get('getHDashBoardData/{startDate}/{endDate}', 'ChartController@getDashBoardDrugData');
 	
 
